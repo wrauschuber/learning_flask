e645a3955af8edea9cc67f0f937450baed122bbf
wrauschuber <114611509+wrauschuber@users.noreply.github.com>
2024-04-04 15:19:42 -0500

    Trouble Shooting

--- app.py
+++ app.py
@@ -2,16 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/mike')
+
+@app.route("/mike")
 def mike():
     n1 = 10
     n2 = 20
     sum = n1 + n2
     return str(sum)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
